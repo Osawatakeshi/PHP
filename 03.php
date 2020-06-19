ccf35c744b3c5b214d688e22bac8586d6fb66b1d
EC2 Default User <ec2-user@ip-172-31-45-171.us-east-2.compute.internal>
2020-06-19 02:23:15 +0000

    second commit

--- 03.php
+++ 03.php
@@ -1,36 +1,31 @@
 <?php
-$name = "Osawa Takeshi";
 
-if ($name = "Osawa Takeshi") {
-    echo "私は $name です" . "\n";
-} 
+$name = 'Osawa Takeshi';
 
-else {
-    echo "$name ではありません" . "\n";
+if ($name = 'Osawa Takeshi') {
+    echo "私は $name です"."\n";
+} else {
+    echo "$name ではありません"."\n";
 }
-
 
 $total = 0;
     echo $total;
-    
-for($i = 0; $i <= 10000; $i++) {
+
+for ($i = 0; $i <= 10000; ++$i) {
     echo $total += $i;
 }
-    echo $total . "\n";
+    echo $total."\n";
 
-
-$fruits = array("pineapple","Kiwi","pear","Muscat","blueberry",) ;
-    
-    foreach($fruits as $fruit) {
-        echo $fruit . "\n";
-}
-
+$fruits = array('pineapple','Kiwi','pear','Muscat','blueberry');
+    foreach ($fruits as $fruit) {
+        echo $fruit."\n";
+    }
 
 $start = 1;
 $end = 100;
 
-for($i = $start; $i <= $end; $i++) {
-  if($i % 5 == 0) {
-    echo $i . "\n";
-  }
+for ($i = $start; $i <= $end; ++$i) {
+    if ($i % 5 == 0) {
+        echo $i."\n";
+    }
 }
